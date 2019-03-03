1d92b58950e1a4d25b427fba54331ae1a5ac34a6
Anmol Varma <anmol_varma@github.com>
2019-03-03 17:45:20 +0530

    Basic wallpaper from template

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello world');
+const Jimp = require('jimp');
+const { list } = require('./mocks')
+
+const xList = [1950, 2510, 2635, 2760, 2900, 3100]
+const file = `${new Date().getTime()}.jpg`;
+
+const writeRowForEachTeam = (image, font, x, y, teamInfo) => {
+    Object.keys(teamInfo).map((eachColum, index) => {
+        image.print(font, x[index], y, teamInfo[eachColum])
+    })
+}
+
+const createTableFromTemplate = async (teamDetails) => {
+    const differenceBetweenRows = 160
+    const image = await Jimp.read('./Assets/Template/template.jpg')
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE)
+    teamDetails.map(async (eachTeam, index) => {
+        await writeRowForEachTeam(image, font, xList, 475 + differenceBetweenRows * index, eachTeam)
+    })
+    image.write(file, () => { })
+}
+
+createTableFromTemplate(list)
---
+++ mocks.js
@@ -0,0 +1,63 @@
+const list = [
+    {
+        name: 'CSK',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },{
+        name: 'SRH',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },{
+        name: 'RCB',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },{
+        name: 'KKR',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },{
+        name: 'MI',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },{
+        name: 'RR',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },{
+        name: 'DD',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },{
+        name: 'KXIP',
+        played: '0',
+        won: '0',
+        lost: '0',
+        runrate: '0.00',
+        points: '0',
+    },
+]
+
+module.exports = {
+    list
+}
