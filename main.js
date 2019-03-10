c79f33313c98f8799414e3906a9fc83df3f8c5d5
Anmol Varma <anmol_varma@github.com>
2019-03-10 17:32:05 +0530

    Image generated

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const Jimp = require('jimp');
 const { list } = require('./mocks')
 
-const xList = [1950, 2510, 2635, 2760, 2900, 3100]
-const file = `${new Date().getTime()}.jpg`;
+const xList = [1950, 2120, 2510, 2635, 2760, 2900, 3100]
+const file = `${new Date().getTime()}.png`;
 
-const writeRowForEachTeam = (image, font, x, y, teamInfo) => {
+const writeRowForEachTeam = async (image, font, x, y, teamInfo) => {
+    const logo = await Jimp.read(`./Assets/Logos/${teamInfo.name.toLowerCase()}.png`)
+    image.composite(logo.resize(Jimp.AUTO, 80), x[0], y)
     Object.keys(teamInfo).map((eachColum, index) => {
-        image.print(font, x[index], y, teamInfo[eachColum])
+        return image.print(font, x[index + 1], y, teamInfo[eachColum])
     })
 }
 
 const createTableFromTemplate = async (teamDetails) => {
     const differenceBetweenRows = 160
-    const image = await Jimp.read('./Assets/Template/template.jpg')
+    let image = await Jimp.read('./Assets/Template/template.png')
     const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE)
-    teamDetails.map(async (eachTeam, index) => {
-        await writeRowForEachTeam(image, font, xList, 475 + differenceBetweenRows * index, eachTeam)
+    const waitForImageToWrite = teamDetails.map(async (eachTeam, index) => {
+        return await writeRowForEachTeam(image, font, xList, 475 + differenceBetweenRows * index, eachTeam)
     })
+    await Promise.all(waitForImageToWrite)
+    console.log('writing final file');
     image.write(file, () => { })
 }
 
